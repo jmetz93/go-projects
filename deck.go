36baf27230306365f6e7cc840f488366c05f05dc
Jacob  Metzinger <jacobmetzinger@Jacobs-MBP.socal.rr.com>
2019-09-18 18:31:36 -0700

    Adding print receiver function for decks - replacing for loop in main

--- deck.go
+++ deck.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 // Create a new type of 'deck'
 // which is a slice of stings
 type deck []string
+
+func (d deck) print() { // receiver function for a deck, d refers to the deck
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := deck{newCard(), newCard()}    // in between curly braces are the elements in the array/slice
 	cards = append(cards, "Six of Spades") // adding element to end of array
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	// for i, card := range cards {
+	// 	fmt.Println(i, card)
+	// }
+	cards.print()
 }
 
 func newCard() string { // need to tell compiler what type of data value will be returned by function
