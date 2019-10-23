c6c06631a90c6854565d9111692fa5cf3355daa2
Jacob  Metzinger <jacobmetzinger@Jacobs-MBP.socal.rr.com>
2019-10-23 14:59:37 -0700

    Working Tests for newDeck function - asserting length, first and last cards - setting up test structure for SaveDeck and ReadDeck functions

--- projects/card-deck-simulator/deck_test.go
+++ projects/card-deck-simulator/deck_test.go
@@ -1,16 +1,33 @@
 package main
 
-func TestNewDeck() {
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
 	// make sure deck is created with x number of cards
-
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
 	// make sure first card in deck is ace of spades
-
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be the Ace of Spades, but got %v", d[0])
+	}
 	// make last card is four of clubs
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card to be the Four of Clubs, but got %v", d[len(d)-1])
+	}
 }
-func TestSaveDeckToFile() {
+func TestSaveDeckAndNewDeckFromFile(t *testing.T) {
+	// delete any files in working directory with name "_decktesting"
 
-}
+	// create deck
 
-func TestNewDeckFromFile() {
+	// save to file
 
+	// load from file
+
+	// assert deck length
+
+	// delete any files with name "_decktesting"
 }
