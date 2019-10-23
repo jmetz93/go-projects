c15000c960d5aefdaaef743fe0040bddcd528680
Jacob  Metzinger <jacobmetzinger@Jacobs-MBP.socal.rr.com>
2019-10-23 16:58:58 -0700

    Adding pointer for struct to update it properly

--- projects/structs/main.go
+++ projects/structs/main.go
@@ -2,18 +2,43 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
 	// jacob := person{"Jacob", "Metzinger"} // one method of declaring structs, assigns values based on order
 	// jacob := person{firstName: "Jacob", lastName: "Metzinger"}
-	var jacob person // creates struct with empty property values, in case of person struct each prop will be empty string
+	// var jacob person // creates struct with empty property values, in case of person struct each prop will be empty string
 	// fmt.Println(jacob)
-	jacob.firstName = "Jacob"
-	jacob.lastName = "Metzinger"
-	fmt.Printf("%+v", jacob) // logs struct with property names and values
+	// jacob.firstName = "Jacob"
+	// jacob.lastName = "Metzinger"
+	// fmt.Printf("%+v", jacob) // logs struct with property names and values
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	jimPointer := &jim // creating pointer to memory address of struct
+	jimPointer.updateFirstName("Jacob")
+	jim.print()
+}
 
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+// *person ensures to use the memory address of the pointer
+func (pointerToPerson *person) updateFirstName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
