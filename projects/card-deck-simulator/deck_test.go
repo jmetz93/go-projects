b24725f58e03cccb70d579ed9c12444dbf0f9833
Jacob  Metzinger <jacobmetzinger@Jacobs-MBP.socal.rr.com>
2019-10-23 14:35:35 -0700

    Setting up structure for test file

---
+++ projects/card-deck-simulator/deck_test.go
@@ -0,0 +1,16 @@
+package main
+
+func TestNewDeck() {
+	// make sure deck is created with x number of cards
+
+	// make sure first card in deck is ace of spades
+
+	// make last card is four of clubs
+}
+func TestSaveDeckToFile() {
+
+}
+
+func TestNewDeckFromFile() {
+
+}
